d2bb5737b31bb5d0b35a73e95528837e4ae28695
Eftal Yurtseven <eftalyurtseven@gmail.com>
2020-05-25 10:54:03 +0300

    post struct fix

--- post.go
+++ post.go
@@ -1,18 +1,42 @@
 package devtoclient
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Post struct {
-	type_of                  string `json:"type_of"`
-	id                       int    `json:"id"`
-	title                    string `json:"title"`
-	description              string `json:"description"`
-	readable_publish_date    string `json:"readable_publish_date"`
-	slug                     string `json:"slug"`
-	path                     string `json:"path"`
-	url                      string `json:"url"`
-	comments_count           int    `json:"comments_count"`
-	positive_reactions_count int    `json:"positive_reactions_count"`
+	TypeOf                 string      `json:"type_of"`
+	ID                     int         `json:"id"`
+	Title                  string      `json:"title"`
+	Description            string      `json:"description"`
+	ReadablePublishDate    string      `json:"readable_publish_date"`
+	Slug                   string      `json:"slug"`
+	Path                   string      `json:"path"`
+	URL                    string      `json:"url"`
+	CommentsCount          int         `json:"comments_count"`
+	PositiveReactionsCount int         `json:"positive_reactions_count"`
+	CollectionID           interface{} `json:"collection_id"`
+	PublishedTimestamp     time.Time   `json:"published_timestamp"`
+	CoverImage             string      `json:"cover_image"`
+	SocialImage            string      `json:"social_image"`
+	CanonicalURL           string      `json:"canonical_url"`
+	CreatedAt              time.Time   `json:"created_at"`
+	EditedAt               interface{} `json:"edited_at"`
+	CrosspostedAt          interface{} `json:"crossposted_at"`
+	PublishedAt            time.Time   `json:"published_at"`
+	LastCommentAt          time.Time   `json:"last_comment_at"`
+	TagList                []string    `json:"tag_list"`
+	Tags                   string      `json:"tags"`
+	User                   struct {
+		Name            string      `json:"name"`
+		Username        string      `json:"username"`
+		TwitterUsername string      `json:"twitter_username"`
+		GithubUsername  string      `json:"github_username"`
+		WebsiteURL      interface{} `json:"website_url"`
+		ProfileImage    string      `json:"profile_image"`
+		ProfileImage90  string      `json:"profile_image_90"`
+	} `json:"user"`
 }
 
 func (c *Client) GetPosts() ([]Post, error) {
